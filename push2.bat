306bec166e373d2d52e9174edb2fad35c6e97174
dangluheng <dangluheng@github.com>
2026-04-14 10:43:17 +0800

    Update: Add baby check, expand details, colors, supplement

--- push2.bat
+++ push2.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d C:\Users\dangl\baby-care-assistant
 "C:\Program Files\Git\bin\git.exe" add .
-"C:\Program Files\Git\bin\git.exe" commit -m "Initial commit: Baby Care Assistant"
+"C:\Program Files\Git\bin\git.exe" commit -m "Update: Add baby check, expand details, colors, supplement"
 "C:\Program Files\Git\bin\git.exe" push -u origin main
 pause
